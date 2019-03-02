7f4a559b032838380e3ac12fd25ea50c56690d67
saidjillo <umurjillo@gmail.com>
2019-03-02 11:53:10 +0300

    added model for storing feed item.

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -7,6 +7,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from . import serializers
 from . import models
@@ -119,3 +121,14 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classess = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields   = ('name', 'email',)
+
+
+class LoginViewSet(viewsets.ViewSet):
+    """checks email and passwords and returns an auth token."""
+
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        """Use the ObtainToken APIview to validate and create a token."""
+
+        return ObtainAuthToken().post(request)
